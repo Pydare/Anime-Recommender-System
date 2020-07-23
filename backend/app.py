735e32d734bc94a8ec9b8ef225ccdef7f984cf52
Pydare <dareyadewumi650@gmail.com>
2020-07-23 21:56:33 +0100

    done with communication between client and server side

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,28 @@
 from flask import Flask, jsonify, abort, make_response, request, url_for
 from content_based_plot import vectorize_similarity, get_recommendations, anime_new
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, support_credentials=True)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route('/recommender/<string:anime>', methods=['GET','POST'])
-@cross_origin(supports_credentials=True)
-def recommend(anime):
+@app.route('/recommender', methods=['GET', 'POST'])
+def recommend():
     try:
+        if request.method == 'POST':
+            anime = request.get_json()['anime']
+        
         cos_sim = vectorize_similarity(anime_new['Synopsis'])
         preds = get_recommendations(anime,cos_sim).tolist()
         preds = {i:preds[i] for i in range(len(preds))}
-        return jsonify( {'predictions': preds}) 
+        return jsonify( preds) 
     except KeyError:
         abort(404)
 
-@app.route('/recommender/', methods=['GET'])
-@cross_origin(supports_credentials=True)
-def get_all():
+@app.route('/', methods=['GET'])
+def get_all(): 
     return "get all"
 
-
 @app.errorhandler(404)
-@cross_origin(supports_credentials=True)
 def not_found(error):
     return make_response(jsonify({'error':'Anime not found'}), 404)
     
--- frontend/anime-ui/src/components/FormComponent.js
+++ frontend/anime-ui/src/components/FormComponent.js
@@ -1,48 +1,60 @@
 import React from 'react';
-import axios from 'axios';
 
 class Form extends React.Component{
 
     constructor(props) {
         super(props);
         this.state = {
-          anime_name: ''
+            anime_name: '',
+            result : {}
         };
     
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+        //this.handleSubmit = this.handleSubmit.bind(this);
+      };
     
     handleChange(event) {
         this.setState({anime_name: event.target.value});
-    }
+    };
 
-    handleSubmit(event){
+    handleSubmit = async (event) => {
         event.preventDefault();
 
-        const post = { anime_name: this.state.name }
-        axios.post(`http://127.0.0.1:5000/recommender/${this.state.anime_name}`, post)
-            .then((res) => {
-                console.log(res.data)
-            }).catch((error) => {
-                console.log(error)
-            });
-
+        const requestOptions = {
+            method: 'POST',
+            mode: 'cors',
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json'},
+            body: JSON.stringify({anime: this.state.anime_name})
+        }
+        const response = await fetch('http://127.0.0.1:5000/recommender',requestOptions)
+        
+        const body = await response.json()
+        this.setState({result:body})
+        console.log(this.state.result)
+        
         this.setState({ anime_name: '' })
-    }
-    
+    };
+
     render(){
-
+        const renderItems = Object.keys(this.state.result).map((key, i) =>{
+            return <li key={i}>{this.state.result[key]}</li>
+          });
         return(
-
-            <form onSubmit={this.handleSubmit}>
-                <span className="formtext">Form</span>
-                <input type="text" placeholder="Enter Anime Name"
-                onChange={this.handleChange}
-                value={this.state.anime_name}
-                required />
-                <button>Go</button>
-            </form>
+            <div className="row">
+                <form onSubmit={this.handleSubmit}  method="POST">
+                    <span className="formtext">Form</span>
+                    <input type="text" placeholder="Enter Anime Name"
+                    onChange={this.handleChange}
+                    value={this.state.anime_name}
+                    required />
+                    <button>Go</button>
+                </form>
+                <div>
+                    <ul>{renderItems}</ul>
+                </div>
+                
+            </div>
+            
         );
     }
     
